1a68a47f7e434859b7ce1f96d6f84784ea75f3d6
elizavetaatis <150339957+elizavetaatis@users.noreply.github.com>
2024-09-02 17:57:45 +0300

    begin Home Page

--- my-app/renderer/pages/Home/Home.tsx
+++ my-app/renderer/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import HStyles from "./Home.module.css";
+import { BsBuildingFill } from "react-icons/bs";
 
 const Home:FC = () => {
 
     return (
         <div className={HStyles.homeContainer}>
-
+            <BsBuildingFill />
         </div>
     );
 }
